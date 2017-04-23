4f176c7340056d05b0de84b7c54f542ff988a771
montusappy <montusappy.gmail.com>
2017-04-23 23:26:10 +0530

    Code Changes

--- src/main/java/com/exampl/controller/TestController.java
+++ src/main/java/com/exampl/controller/TestController.java
@@ -3,6 +3,7 @@ package com.exampl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import com.exampl.service.StudentService;
 public class TestController {
 
 	@Autowired
+	@Qualifier("studentServiceImpl")
 	StudentService studService;
 
 	@RequestMapping(method = RequestMethod.GET)
